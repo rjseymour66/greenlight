a100d9e907eb94dd0213a63b72ab5b826c002945
rseymour <rjs.github@gmail.com>
2023-07-03 23:56:58 -0400

    updating a movie

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -94,7 +94,21 @@ func (m *MovieModel) Get(id int64) (*Movie, error) {
 
 // Update updates the movie.
 func (m *MovieModel) Update(movie *Movie) error {
-	return nil
+	query := `
+		UPDATE movies
+		SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+		WHERE id = $5
+		RETURNING version`
+
+	args := []any{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
 }
 
 // Delete deletes the movie.
