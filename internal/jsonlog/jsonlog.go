97859a6877f558f6b71fb1f4c4623b40994c23c9
rseymour <rjs.github@gmail.com>
2023-07-20 23:57:45 -0400

    implementing logger, not complete

---
+++ internal/jsonlog/jsonlog.go
@@ -0,0 +1,46 @@
+package jsonlog
+
+import (
+	"io"
+	"sync"
+)
+
+type Level int8
+
+// Initialize constants which represent a specific severity level. We use the iota
+// keyword as a shortcut to assign successive integer values to the constants.
+const (
+	LevelInfo  Level = iota // Has the value 0.
+	LevelError              // Has the value 1.
+	LevelFatal              // Has the value 2.
+	LevelOff                // Has the value 3.
+)
+
+func (l Level) String() string {
+	switch l {
+	case LevelInfo:
+		return "INFO"
+	case LevelError:
+		return "ERROR"
+	case LevelFatal:
+		return "FATAL"
+	default:
+		return ""
+	}
+}
+
+// Logger is a custom logger type. This holds the output destination that the log entries
+// will be written to, the minimum severity level that log entries will be written for,
+// plus a mutex for coordinating the writes.
+type Logger struct {
+	out      io.Writer
+	minLevel Level
+	mu       sync.Mutex
+}
+
+func New(out io.Writer, minLevel Level) *Logger {
+	return &Logger{
+		out:      out,
+		minLevel: minLevel,
+	}
+}
