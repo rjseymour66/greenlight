7244db3c63202d7603f7fa45eb36f2e9577fb5ec
rseymour <rjs.github@gmail.com>
2023-07-21 00:18:37 -0400

    structured json log entries

--- internal/jsonlog/jsonlog.go
+++ internal/jsonlog/jsonlog.go
@@ -1,8 +1,12 @@
 package jsonlog
 
 import (
+	"encoding/json"
 	"io"
+	"os"
+	"runtime/debug"
 	"sync"
+	"time"
 )
 
 type Level int8
@@ -38,9 +42,63 @@ type Logger struct {
 	mu       sync.Mutex
 }
 
+// New Logger instance which writes log entries at or above a minimum severity
+// level to a specific output destination.
 func New(out io.Writer, minLevel Level) *Logger {
 	return &Logger{
 		out:      out,
 		minLevel: minLevel,
 	}
+}
+
+func (l *Logger) PrintInfo(message string, properties map[string]string) {
+	l.print(LevelInfo, message, properties)
+}
+
+func (l *Logger) PrintError(err error, properties map[string]string) {
+	l.print(LevelError, err.Error(), properties)
+}
+
+func (l *Logger) PrintFatal(err error, properties map[string]string) {
+	l.print(LevelFatal, err.Error(), properties)
+	os.Exit(1)
+}
+
+func (l *Logger) print(level Level, message string, properties map[string]string) (int, error) {
+	if level < l.minLevel {
+		return 0, nil
+	}
+
+	aux := struct {
+		Level      string            `json:"level"`
+		Time       string            `json:"time"`
+		Message    string            `json:"message"`
+		Properties map[string]string `json:"properties,omitempty"`
+		Trace      string            `json:"trace,omitempty"`
+	}{
+		Level:      level.String(),
+		Time:       time.Now().UTC().Format(time.RFC3339),
+		Message:    message,
+		Properties: properties,
+	}
+
+	if level >= LevelError {
+		aux.Trace = string(debug.Stack())
+	}
+
+	var line []byte
+
+	line, err := json.Marshal(aux)
+	if err != nil {
+		line = []byte(LevelError.String() + ": unable to marshal log message: " + err.Error())
+	}
+
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	return l.out.Write(append(line, '\n'))
+}
+
+func (l *Logger) Write(message []byte) (n int, err error) {
+	return l.print(LevelError, string(message), nil)
 }
